73166791ed5b7bd3ceb17dd9abc49d5812dd287f
Jake Frischmann <jjfrisch@outlook.com>
2022-12-10 22:59:41 -0600

    Update Runner.java

--- Runner.java
+++ Runner.java
@@ -2,14 +2,15 @@
 public class Runner {
 
 	public static void main(String[] args) {
-		String val = "ioenxxxqropr..o02f,34lf.l"; //used to encript
-		PlayFairCypherRunner x = new PlayFairCypherRunner(val); //create a new cypher
-		String Em = x.encrypt("xanthinars guide is worderous!"); //encripting the string
-		String message = x.decrypt(Em); //decripting the resulting string (used the encripting string at the top.)
+		String key_val = "ioenxxxqropr..o02f,34lf.l"; //used to encript
+		String message = "xanthinars guide is worderous!"; //text to encript
+		PlayFairCypherRunner x = new PlayFairCypherRunner(key_val); //create a new cypher
+		String encripted_message = x.encrypt(message); //encripting the string
+		String decripted_message = x.decrypt(encripted_message); //decripting the resulting string (used the encripting string at the top.)
 		
-        System.out.println("This string is encripted: xanthinars guide is worderous!");
-		System.out.println("Turns into this: " + Em);
-		System.out.println("decripted back into the original with no spaces: " + message);
+        System.out.println("This string is encripted: " + message);
+		System.out.println("Turns into this: " + encripted_message);
+		System.out.println("Decripted back into the original with no spaces: " + decripted_message);
 		
 
 	}
